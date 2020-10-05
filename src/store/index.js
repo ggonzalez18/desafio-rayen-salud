5cca7d398819f97d05c18b1f750cf1749f47ba3b
Gabriela Gonzalez <gabytasmile7@gmail.com>
2020-10-05 19:08:51 -0400

    llamada primer endpoint

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,28 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    tutorial: undefined,
+    tutorials: []
   },
   mutations: {
+    GET_TUTORIAL(state, tutorial) {
+      state.tutorial = tutorial
+    },
+    GET_TUTORIALS(state, tutorials) {
+      state.tutorials = tutorials
+    }
   },
   actions: {
+    getTutorials({ commit }) {
+      Axios.get('https://rayentutorialtestapp.azurewebsites.net/tutorials').then((response) => {
+        commit('GET_TUTORIALS', response.data)
+      })
+    },
   },
   modules: {
   }
--- src/views/Tutoriales.vue
+++ src/views/Tutoriales.vue
@@ -1,17 +1,30 @@
 <template>
   <div class="tutorial">
     <nav-tutoriales></nav-tutoriales>
+    <v-container>
+      <p>{{tutorials}}</p>
+    </v-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import { mapActions, mapState } from 'vuex'
 import NavTutoriales from '@/components/NavTutoriales.vue'
+
 
 export default {
   name: 'tutoriales',
   components: {
     NavTutoriales,
-  }
+  },
+  created() {
+    this.getTutorials()
+  },
+  methods: {
+    ...mapActions(['getTutorials'])
+  },
+  computed: {
+    ...mapState(['tutorials'])
+  },
 }
 </script>
