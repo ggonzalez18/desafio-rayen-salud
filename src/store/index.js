56b680e2a6c04ec0efb033f048669ed5b61a80ae
Gabriela Gonzalez <gabytasmile7@gmail.com>
2020-10-05 21:57:48 -0400

    se avanzo en formulario para agregar

--- src/store/index.js
+++ src/store/index.js
@@ -7,7 +7,10 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     tutorial: undefined,
-    tutorials: []
+    tutorials: [],
+    items: ['Titulo', 'Fecha'],
+    selectedItem: undefined,
+    searchDescription: undefined
   },
   mutations: {
     GET_TUTORIAL(state, tutorial) {
@@ -15,7 +18,13 @@ export default new Vuex.Store({
     },
     GET_TUTORIALS(state, tutorials) {
       state.tutorials = tutorials
-    }
+    },
+    SET_ITEM(state, newItem) {
+      state.selectedItem = newItem
+    },
+    SET_DESCRIPTION(state, newDescription) {
+      state.searchDescription = newDescription
+    },
   },
   actions: {
     getTutorials({ commit }) {
@@ -23,9 +32,18 @@ export default new Vuex.Store({
         commit('GET_TUTORIALS', response.data)
       })
     },
+    setItem({ commit, state }, newItem) {
+      commit('SET_ITEM', newItem)
+      return state.selectedItem
+    },
     deleteTutorials({ dispatch }) {
       Axios.delete('https://rayentutorialtestapp.azurewebsites.net/deletetutorials').then(() => {
         dispatch('getTutorials')
+      })
+    },
+    filterTutorials({ commit }, newDescription) {
+      Axios.get(`https://rayentutorialtestapp.azurewebsites.net/tutorial?description=${newDescription}`).then((response) => {
+        commit('SET_DESCRIPTION', response.data)
       })
     },
   },
--- src/views/Agregar.vue
+++ src/views/Agregar.vue
@@ -1,6 +1,63 @@
 <template>
+
   <div class="tutorial">
     <nav-agregar></nav-agregar>
+    <v-container>
+      <v-form
+    ref="form"
+    v-model="valid"
+    lazy-validation
+  >
+    <v-text-field
+      v-model="titulo"
+      :counter="10"
+      :rules="tituloRules"
+      label="Titulo"
+      required
+    ></v-text-field>
+        <v-text-field
+      v-model="profesor"
+      :counter="10"
+      label="Profesor"
+      required
+    ></v-text-field>
+        <v-text-field
+      v-model="materia"
+      :counter="10"
+      label="Materia"
+      required
+    ></v-text-field>
+       <v-text-field
+      v-model="fecha"
+      label="Fecha"
+      required
+    ></v-text-field>
+
+    <v-btn
+      :disabled="!valid"
+      color="success"
+      class="mr-4"
+      @click="validate"
+    >
+      Validate
+    </v-btn>
+
+    <v-btn
+      color="error"
+      class="mr-4"
+      @click="reset"
+    >
+      Reset Form
+    </v-btn>
+
+    <v-btn
+      color="warning"
+      @click="resetValidation"
+    >
+      Reset Validation
+    </v-btn>
+     </v-form>
+     </v-container>
   </div>
 </template>
 
@@ -12,6 +69,31 @@ export default {
   name: 'Agregar',
   components: {
     NavAgregar,
-  }
+  },
+  data: () => ({
+      valid: true,
+      titulo: '',
+      tituloRules: [
+        v => !!v || 'titulo',
+        v => (v && v.length <= 10) || 'titulo no puede exceder los 10 caracteres',
+      ],
+      Profesor: '',
+      profesorRules: [
+        v => !!v || 'titulo',
+        v => (v && v.length <= 10) || 'titulo no puede exceder los 10 caracteres',
+      ],
+    }),
+
+    methods: {
+      validate () {
+        this.$refs.form.validate()
+      },
+      reset () {
+        this.$refs.form.reset()
+      },
+      resetValidation () {
+        this.$refs.form.resetValidation()
+      },
+    },
 }
 </script>
